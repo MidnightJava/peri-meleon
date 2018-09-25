b9b7a00048c0dc587f13ca8641c64a219197df0a
Frederick Kuhl <fkuhl@tyndalesoft.com>
2018-09-25 14:30:24 -0400

    Age queries corrected using newer date-time classes.

--- src/com/tamelea/pm/data/MemberAgeFilter.java
+++ src/com/tamelea/pm/data/MemberAgeFilter.java
@@ -1,6 +1,7 @@
 package com.tamelea.pm.data;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.MonthDay;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -25,54 +26,59 @@ public class MemberAgeFilter implements MemberFilter {
 		this.age = age;
 		this.activeOnly = activeOnly;
 	}
-
+	
 	public boolean match(MemberIndex index) {
 		if (activeOnly){
 			if (!data.isActive(index)) return false;
 		}
 		PMDate dob = (PMDate)data.getMemberValue(index, MemberField.DATE_OF_BIRTH);
 		if (dob.getValue() == null) return false;
-		GregorianCalendar dobValue = new GregorianCalendar();
-		dobValue.setTime(dob.getValue());
-		dobValue.set(Calendar.HOUR_OF_DAY, 0);
-		dobValue.set(Calendar.MINUTE, 0);
-		Date dobDate = new Date(dobValue.getTimeInMillis());
-		GregorianCalendar dobPlusOneYearValue = new GregorianCalendar();
-		dobPlusOneYearValue.setTime(dobDate);
-		dobPlusOneYearValue.add(Calendar.YEAR, 1);
-		dobPlusOneYearValue.add(Calendar.DAY_OF_YEAR, -1);
-		dobPlusOneYearValue.set(Calendar.HOUR_OF_DAY, 23);
-		dobPlusOneYearValue.set(Calendar.MINUTE, 59);
-		dobPlusOneYearValue.set(Calendar.SECOND, 59);
-		Date dobPlusOneYearDate = new Date(dobPlusOneYearValue.getTimeInMillis());
-		GregorianCalendar baseDate = new GregorianCalendar();
-		baseDate.setTime(date.getValue());
-		baseDate.add(Calendar.YEAR, -age);
-		baseDate.set(Calendar.HOUR_OF_DAY, 0);
-		baseDate.set(Calendar.MINUTE, 0);
-		Date compareDate = new Date(baseDate.getTimeInMillis());
+		int membersAge = membersConventionalAge(dob.getValue());
 		switch (comparisonOp){
 			case LESS:
-				return dobDate.after(compareDate);
+				return membersAge < age;
 			case LESS_EQUAL:
-				return (dobDate.after(compareDate) ||  dateInBirthYear(compareDate, dobDate, dobPlusOneYearDate));
+				return membersAge <= age;
 			case GREATER:
-				return dobDate.before(compareDate);
+				return membersAge > age;
 			case GREATER_EQUAL:
-				return (dobDate.before(compareDate) ||  dateInBirthYear(compareDate, dobDate, dobPlusOneYearDate));
+				return membersAge >= age;
 			case EQUAL:
-				return dateInBirthYear(compareDate, dobDate, dobPlusOneYearDate);
+				return membersAge == age;
+			default:
+				return false;
 		}
-		return false;
+	}
+	
+	/**
+	 * Compute a member's "conventional age", which is incremented
+	 * on their birthday.
+	 * @param storedDob date of birth stored for member
+	 * @return conventional age
+	 */
+	private int membersConventionalAge(Date storedDob) {
+		GregorianCalendar storedCalendar = new GregorianCalendar();
+		storedCalendar.setTime(storedDob);
+		//Escape Early Java Calendar Hell
+		LocalDate dob = LocalDate.of(
+				storedCalendar.get(GregorianCalendar.YEAR), 
+				storedCalendar.get(GregorianCalendar.MONTH) + 1, //definitions differ!
+				storedCalendar.get(GregorianCalendar.DAY_OF_MONTH));
+		LocalDate todaysDate = LocalDate.now();
+		MonthDay dobMonthDay = MonthDay.of(dob.getMonth(), dob.getDayOfMonth());
+		int age = 0;
+		if (MonthDay.now().isBefore(dobMonthDay)) {
+			//before birthday
+			age = todaysDate.getYear() - dob.getYear() - 1;
+		} else {
+			//on or after birthday
+			age = todaysDate.getYear() - dob.getYear();
+		}
+		return age;
 	}
 	
 	public PMDate getDate(){
 		return date;
-	}
-	
-	private boolean dateInBirthYear(Date compareDate, Date dob, Date dobPlusOneYear) {
-		return ((dob.before(compareDate) || dob.equals(compareDate)) && 
-				(dobPlusOneYear.after(compareDate) || dobPlusOneYear.equals(compareDate)));
 	}
 	
 	public Integer getAge(){
